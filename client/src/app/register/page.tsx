0af4070a2110d712f07a5cb0e709170d58e3e185
WilliamGMG <williamglynn5@gmail.com>
2025-10-20 15:01:34 -0400

    Created a basic structure for the dashboard.

--- client/src/app/register/page.tsx
+++ client/src/app/register/page.tsx
@@ -10,7 +10,7 @@ export default function RegisterPage() {
     return (
         <html><body>
             <main className='register-container'>
-                
+
                 <HeroSection />
                 <RegisterForm />
             </main>
@@ -28,9 +28,9 @@ function HeroSection() {
             </p>
 
             <div className='stock-line'>
-                <AiOutlineStock size={400} className='stock-icon'/>
+                <AiOutlineStock size={400} className='stock-icon' />
             </div>
-            
+
             <div className='red-to-green-container'>
                 <span className='red-text'>Red</span>
                 <span className='two-text'>2</span>
@@ -48,11 +48,11 @@ function RegisterForm() {
         <div className='register-form-container'>
             <h1 className='register-form-header'>Create an account</h1>
             <form className='register-form'>
-                <input type='first-name' placeholder='First Name'/>
-                <input type='last-name' placeholder='Last Name'/>
-                <input type='email' placeholder='user@email.com'/>
+                <input type='first-name' placeholder='First Name' />
+                <input type='last-name' placeholder='Last Name' />
+                <input type='email' placeholder='user@email.com' />
                 <div className='password-container'>
-                    <input 
+                    <input
                         type={showPassword ? 'text' : 'password'}
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
@@ -64,7 +64,7 @@ function RegisterForm() {
                         <button
                             type='button'
                             className='hide-password-icon'
-                            onClick={() => setShowPassword(!showPassword)}    
+                            onClick={() => setShowPassword(!showPassword)}
                         >
                             {showPassword ? <RiEyeOffFill /> : <RiEyeFill />}
                         </button>
