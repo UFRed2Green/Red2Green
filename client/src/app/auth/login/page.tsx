bedd644966df94a34d9b23f2306a8ffca7a11fd4
cibarbia05 <christianibarbia2015@gmail.com>
2025-10-26 08:59:30 -0400

    Fix login page space to the right

--- client/src/app/auth/login/page.tsx
+++ client/src/app/auth/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import '@/app/styles/login.css'
+import '@/app/styles/login.css';
 import { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
@@ -9,85 +9,17 @@ import { RiEyeFill, RiEyeOffFill } from "react-icons/ri";
 import { useAuth } from "@/context/AuthContext";
 
 export default function LoginPage() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const { login } = useAuth();
-  const router = useRouter();
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setIsLoading(true);
-
-    try {
-      await login(email, password);
-      // Redirect to dashboard on successful login
-      router.replace("/dashboard/dashboard");
-    } finally {
-      setIsLoading(false);
-    }
-  };
-
   return (
-    <div className="container">
-      {/* LEFT PANEL */}
+    <main className="login-container">
       <HeroSection />
-
-      {/* RIGHT PANEL */}
-      <div className="right">
-        <div className="form-container">
-          <h2>Login</h2>
-          <form className='login-form' onSubmit={handleSubmit}>
-            <h3>Email</h3>
-            <div className='email-container'>
-              <input
-                type="email"
-                placeholder="user@email.com"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                disabled={isLoading}
-              />
-            </div>
-
-            <h3>Password</h3>
-            <div className='password-container'>
-              <input
-                type={showPassword ? 'text' : 'password'}
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className='password-input'
-                placeholder='••••••••••'
-                required
-                disabled={isLoading}
-              />
-              <span
-                className={`hide-password-icon ${showPassword ? 'active' : ''}`}
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                { password.length > 0 && (showPassword ? <RiEyeOffFill /> : <RiEyeFill />)}
-              </span>
-            </div>
-
-            <button type="submit" disabled={isLoading}>
-              {isLoading ? "Logging in..." : "Login"}
-            </button>
-          </form>
-          <div className="auth-links">
-            <Link href="/auth/forgotPassword" className="forgot-password-link">
-              Forgot Password?
-            </Link>
-          </div>
-        </div>
-      </div>
-    </div>
+      <LoginForm />
+    </main>
   );
 }
 
 function HeroSection() {
   return (
-    <div className="register-hero-section-container">
+    <div className="login-hero-section-container">
       <p className="welcome-text">
         The best way to track your profits.
         <br />
@@ -102,6 +34,77 @@ function HeroSection() {
         <span className="red-text">Red</span>
         <span className="two-text">2</span>
         <span className="green-text">Green</span>
+      </div>
+    </div>
+  );
+}
+
+function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const { login } = useAuth();
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      await login(email, password);
+      router.replace("/dashboard/dashboard");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="login-form-container">
+      <h1 className="login-form-header">Login</h1>
+      <form className="login-form" onSubmit={handleSubmit}>
+        <h3>Email</h3>
+        <input
+          type="email"
+          placeholder="user@email.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          disabled={isLoading}
+        />
+        
+        <h3>Password</h3>
+        <div className="password-container">
+          <input
+            type={showPassword ? 'text' : 'password'}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="password-input"
+            placeholder="••••••••••"
+            required
+            disabled={isLoading}
+          />
+
+          {password.length > 0 && (
+            <button
+              type="button"
+              className="hide-password-icon"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? <RiEyeOffFill /> : <RiEyeFill />}
+            </button>
+          )}
+        </div>
+
+        <button className="login-button" type="submit" disabled={isLoading}>
+          {isLoading ? "Logging in..." : "Login"}
+        </button>
+      </form>
+
+      <div className="auth-links">
+        <Link href="/auth/forgotPassword" className="forgot-password-link">
+          Forgot Password?
+        </Link>
       </div>
     </div>
   );
